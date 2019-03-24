046c867d367307f1c73886a81daea43dd8257ef8
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-03-24 21:13:59 +0400

    Create add_lxd_lxdgroup.sh

---
+++ add_lxd_lxdgroup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /etc/zabbix
+sudo git clone https://github.com/ampintilie/LXD_Monitor
+sudo cp -R /etc/zabbix/LXD_Monitor/scripts /etc/zabbix/lxd_external_scripts
+sudo chmod +x /etc/zabbix/lxd_external_scripts/*
+sudo cp LXD_Monitor/zabbix_agentd.conf.d/lxd.conf /etc/zabbix/zabbix_agentd.d/lxd.conf
+sudo usermod -a -G lxd zabbix
+sudo systemctl restart zabbix-agent.service
+sudo rm -rf /etc/zabbix/LXD_Monitor
