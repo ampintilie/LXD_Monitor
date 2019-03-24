53f9c55c7728dc203e0b46569dce0f0c2a9a229d
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-03-24 21:15:14 +0400

    Update run_lxd.sh

--- run_lxd.sh
+++ run_lxd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-juju scp add_lxd.sh 0:/home/ubuntu
-juju run --machine 0 "chmod +x /home/ubuntu/add_lxd.sh"
-juju run --machine 0 "~/add_lxd.sh"
+juju scp add_lxd_lxdgroup.sh 0:/home/ubuntu
+juju run --machine 0 "chmod +x /home/ubuntu/add_lxd_lxdgroup.sh"
+juju run --machine 0 "~/add_lxd_lxdgroup.sh"
