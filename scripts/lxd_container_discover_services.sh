eb8e086e1aeb1e8843ff5761a6dd76aa1c1b0018
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-04-16 21:33:46 +0300

    Update lxd_container_discover_services.sh

--- scripts/lxd_container_discover_services.sh
+++ scripts/lxd_container_discover_services.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 container=`echo $1|awk -F '.' '{print $2}'`
 
-
-services=`lxc exec $container -- systemctl list-units -t service | grep "neutron\|nova\|ovsdb\|ceph\|cinder\|glance\|keystone\|apache2\|mysql\|percona-cluster\|rabbitmq-server\|gnocchi\|heat" | grep -v "juju" | grep -o "\w.*.service"`
+services=`lxc exec $container -- systemctl list-units -t service | grep "barbican\|ceilometer\|ceph\|cinder\|etcd\|glance\|gnocchi\|heat\|keystone\|magnum\|memcached\|neutron\|nova\|ovsdb\|apache2\|mysql\|rabbitmq-server\|vault" | grep -v "juju" | grep -o "\w.*.service"`
 
 echo "{"
 echo "     \"data\":["
