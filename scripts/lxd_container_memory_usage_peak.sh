13e7c585d7aea5bbde0ecab99351a62b112c1330
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-04-17 14:32:16 +0300

    Update lxd_container_memory_usage_peak.sh
    resolved error: "became not supported: Value xxxxxxxxxx.xx is too small or too large"

--- scripts/lxd_container_memory_usage_peak.sh
+++ scripts/lxd_container_memory_usage_peak.sh
@@ -9,14 +9,13 @@ mem=`lxc info $container |awk '/peak/ {print $3}'`
 if echo $mem |grep GB > /dev/null
   then
    number=`lxc info $container | awk '/peak/ {print $3}'|head --bytes -3`
-   # print usage in Bytes
-   echo "($number * 1024 * 1024 * 1024)"|bc
+   # print usage in MBytes
+   echo "($number * 1024)"|bc
 fi
 
 if echo $mem |grep MB > /dev/null
-  then 
+  then
    number=`lxc info $container | awk '/peak/ {print $3}'|head --bytes -3`
-   # print usage in Bytes
-   echo "($number * 1024 * 1024)"|bc
+   # print usage in MBytes
+   echo "$number"|bc
 fi
-
