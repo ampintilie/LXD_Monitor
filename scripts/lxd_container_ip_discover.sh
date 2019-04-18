df336cbb3ce6333f498ed56c75df9f5e92c50a15
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-04-18 12:55:57 +0300

    Create lxd_container_ip_discover.sh

---
+++ scripts/lxd_container_ip_discover.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+container=`echo $1|awk -F '.' '{print $2}'`
+
+ips=`lxc list $container$ --columns=4 | grep eth0 | awk '{print $2}'`
+
+echo "{"
+echo "     \"data\":["
+
+
+
+
+
+
+comma=""
+for ip in $ips
+do
+    echo "     $comma{"
+    echo "           \"{#IPV4}\":\"$ip\""
+    echo "     }"
+    comma=","
+done
+
+
+
+
+
+
+echo "     ]"
+echo "}"
