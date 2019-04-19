64caceb981463450d8938afd6091a7e63952d7d7
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-04-19 14:54:06 +0300

    Update get_ceph_storage.sh

--- scripts/get_ceph_storage.sh
+++ scripts/get_ceph_storage.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-containerId="juju-143605-0-lxd-8"
-value=`lxc exec $containerId -- sh -c "ceph df -f json-pretty" | grep "$1" | awk '{print $2}' | sed 's/,//'`
+value=`lxc exec "$2" -- sh -c "ceph df -f json-pretty" | grep "$1" | awk '{print $2}' | sed 's/,//'`
 expr $value / 1024 / 1024 / 1024
